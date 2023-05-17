9cecf83c11e63fe6c1d822ab9d608f0d3235ec0f
Shehzaad Saifulla <shehzaad.saifulla@einride.tech>
2023-05-17 09:46:11 +0200

    feat(docs-pdf): add logging and comments

--- drivepdf.js
+++ drivepdf.js
@@ -1,22 +1,27 @@
 function main() {
-	var folder = DriveApp.getFolderById('<drive_folder_id>') // Insert Google Drive folder ID here
-	var now = new Date()
-	var pdfFolder = DriveApp.createFolder('Coverted Docs ' + now) // Folder to store the converted documents
-	convert(folder, pdfFolder)
+	const docsFolder = DriveApp.getFolderById('<drive_folder_id>') // Insert Google Drive folder ID for the folder with Docs/Sheets
+	const now = new Date()
+	const pdfFolder = DriveApp.createFolder('Coverted PDF Files ' + now) // Create new folder to store the converted documents
+	convert(docsFolder, pdfFolder)
 }
 
-function convert(folder, pdfFolder) {
+function convert(docsFolder, pdfFolder) {
 	var file
 	var blob
 	var pdf
-	var fileList = folder.getFiles()
+	const fileList = docsFolder.getFiles() // Get all files in the folder with Docs/Sheets
+
+  Logger.log("Converting files in %s and adding them to new folder %s", docsFolder.getName(), pdfFolder.getName())
 
 	while(fileList.hasNext()){
 		file = fileList.next()
-		if(file.getMimeType() === MimeType.GOOGLE_DOCS || file.getMimeType() === MimeType.GOOGLE_SHEETS){
+
+		if(file.getMimeType() === MimeType.GOOGLE_DOCS || file.getMimeType() === MimeType.GOOGLE_SHEETS){ // Check whether file is a Google Doc or Google Sheet
+      Logger.log("Converting %s", file.getName())
 			blob = file.getBlob()
 			pdf = blob.getAs('application/pdf')
 			pdfDoc = pdfFolder.createFile(pdf)
 		}
 	}
+  Logger.log("Conversion complete!")
 }
